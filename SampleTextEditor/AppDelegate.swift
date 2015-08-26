eb88f0fad987a56bbbbefabe873f9071ba4cca33
ducbao414 <md231@MacBook-Air.local>
2015-08-26 20:38:44 +0700

    Initial Commit

---
+++ SampleTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SampleTextEditor
+//
+//  Created by bmbhh on 8/26/15.
+//  Copyright (c) 2015 bmbhh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
